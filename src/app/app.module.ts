82c362a2f795040da113cc32edd4e74c3897e3be
Damien Springuel <dspringuel@vendasta.com>
2017-10-23 13:36:02 -0600

    Created some routed page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TestpageComponent } from './testpage/testpage.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomepageComponent},
+  { path: 'testpage', component: TestpageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestpageComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
